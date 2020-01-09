b92a335acd0eb3a71c416d0393d8c47657e73ca0
bRealist <55361022+bRealist@users.noreply.github.com>
2020-01-09 17:46:39 +0200

    laba 7

---
+++ Laboratorna 7/zavd 4 - lab 7/Задание 4 (лаба 7)/Smartphone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_4__лаба_7_
+{
+    class Smartphone : ICallProperty, IBrowseProperty
+    {
+        public delegate void SmartphoneHandler(SmartphoneEventArgs sea);
+        public event SmartphoneHandler Notify;
+
+        public Smartphone() { }
+
+        public void Call(string number) => Notify?.Invoke(new SmartphoneEventArgs($"Calling... {number}"));
+        public void Browse(string url)
+        {
+            bool IsDigit = false;
+            for(int i=0;i<url.Length;i++)
+            {
+                IsDigit = int.TryParse(Convert.ToString(url[i]),out int n);
+                if (IsDigit) { Notify?.Invoke(new SmartphoneEventArgs("Invalid URL!")); break; }
+            }
+            if (!IsDigit) { Notify?.Invoke(new SmartphoneEventArgs($"Browsing: {url}!")); }
+        }
+    }
+}
---
+++ Laboratorna 7/zavd 5 - lab 7/Задание 5 (лаба 7)/IIdentifiable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_5__лаба_7_
+{
+    interface IIdentifiable
+    {
+        string Id { get; set; }
+
+        public void FindFakeId(string s);
+    }
+}
