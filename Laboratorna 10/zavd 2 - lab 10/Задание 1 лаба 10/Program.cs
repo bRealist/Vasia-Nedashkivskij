46d69f44e7d01f0492977e91a37cb2260cda1646
bRealist <55361022+bRealist@users.noreply.github.com>
2020-01-09 17:50:15 +0200

    laba 10

---
+++ Laboratorna 10/zavd 2 - lab 10/Задание 1 лаба 10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+namespace Задание_1_лаба_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> someBody = new ListyIterator<string>();
+
+            string[] command = Console.ReadLine().Split(" ");
+
+            if (command[0] == "Create" && command.Length > 1)
+            { someBody.Create(command); }
+            
+            while(command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+                try
+                {
+                    if (command[0] == "Move")
+                    { Console.WriteLine(someBody.Move()); }
+                    else if (command[0] == "HasNext")
+                    { Console.WriteLine(someBody.HasNext()); }
+                    else if (command[0] == "Print")
+                    { someBody.Print(); }
+                    else if (command[0] == "PrintAll")
+                    {
+                       foreach(string item in someBody.PrintAll())
+                        {
+                            Console.Write(item + " ");
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Operation!");
+                }
+            }
+        }
+    }
+}
