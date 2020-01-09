87934db87723f2f21d770bb2e685923871b910be
bRealist <55361022+bRealist@users.noreply.github.com>
2020-01-09 17:49:23 +0200

    laba 9

---
+++ Laboratorna 9/zavd 4 - lab 9/задание 4 лабка 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace задание_3_лаба_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int s;
+            int[] bS = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                s = int.Parse(Console.ReadLine());
+                bS[i] = s;
+            }
+
+            Box<int> someBox = new Box<int>(n, bS);
+
+            string[] command = Console.ReadLine().Split(" ");
+
+            Swap<int>(someBox, int.Parse(command[0]), int.Parse(command[1]));
+        }
+
+        public static void Swap<T>(Box<T> Props, int i, int j)
+        {
+            T t = Props.SomeProps[i];
+            Props.SomeProps[i] = Props.SomeProps[j];
+            Props.SomeProps[j] = t;
+
+            Props.Display();
+        }
+    }
+}
